65bb6e46354220e40f382f7dcd8a6c92d1edfe87
kaltrinaav <kaltrinaveseliii@outlook.com>
2017-11-24 14:32:28 +0100

    added - json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-	console.log("DOM is ready!")
+	var datatimeNow = new Date();
+	$('#datetime-now').val(datatimeNow);
+	console.log("DOM is ready!",datatimeNow)
+
 	$('#border-wait-form').submit(function(event){
 		event.preventDefault();
 		console.log("Form!")
 
+		var datetimeNow = $('#datetime-now').val();
 		var pikaKufitare = $('#pika-kufitare').val();
 		//Hyrje ne minuta
 		var hyrjeMinMinuta = $('#hyrje-min-minuta').val();
@@ -19,13 +23,28 @@ $(document).ready(function(){
 		var daljeMinMetra = $('#dalje-min-metra').val();
 		var daljeMaxMetra = $('#dalje-max-metra').val();
 
-		console.log("1 Pika kufitare:",pikaKufitare,'\n',"2 Hyrje min:",hyrjeMinMinuta,
+		var dataJson = {"pikaKufitare": pikaKufitare,
+                        "hyrjeMinMinuta" : parseInt(hyrjeMinMinuta),
+                        "hyrjeMaxMinuta" : parseInt(hyrjeMaxMinuta),
+                        "daljeMinMinuta" : parseInt(daljeMinMinuta),
+                        "daljeMaxMinuta" : parseInt(daljeMaxMinuta),
+                        "hyrjeMinMetra" : parseInt(hyrjeMinMetra),
+                        "hyrjeMaxMetra" : parseInt(hyrjeMaxMetra),
+                        "daljeMinMetra" : parseInt(daljeMinMetra),
+                        "daljeMaxMetra" : parseInt(daljeMaxMetra),
+                        "datetimeNow" : new Date()
+                        };
+
+		console.log(dataJson);
+
+
+		/*console.log("1 Pika kufitare:",pikaKufitare,'\n',"2 Hyrje min:",hyrjeMinMinuta,
 			'\n',"3 Hyrje max:",hyrjeMaxMinuta,'\n'," 4 Dalje min:",
 			daljeMinMinuta,'\n'," 5 Dalje max:",daljeMaxMinuta,
 			'\n',"6 Hyrje min ",hyrjeMinMetra,'\n',"7 Hyrje max:",hyrjeMaxMetra,
 			'\n',"8 Dalje min:",daljeMinMetra,'\n',"9 Dalje max:",daljeMaxMetra
 
-			);
+			); */
 		//console.log(hyrjeMinMinuta," hyrja min ,",hyrjeMaxMinuta," hyrja max");
 	});
 });
